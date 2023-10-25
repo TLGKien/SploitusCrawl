57d90c7b241bf74d2108415de73b5d5d902be5dd
tlgkien <kienmathip1999@gmail.com>
2023-10-24 17:48:51 -0700

    add search_module and show_options

--- 2.Code/Kiennn/metasploit/msfrpc/pymsfrpc/msfrpc.py
+++ 2.Code/Kiennn/metasploit/msfrpc/pymsfrpc/msfrpc.py
@@ -15,11 +15,43 @@ class Msfrpc:
       self.msg = msg
     
   def __init__(self,opts=[]):    
+    """
+    Initialize an instance of the Msfrpc client.
+
+    Args:
+        opts (dict, optional): A dictionary of options to configure the client.
+            Supported options:
+            - 'host' (str): The host where the Metasploit RPC service is running.
+            - 'port' (int): The port to connect to the RPC service.
+            - 'uri' (str): The URI path for the RPC service.
+            - 'ssl' (bool): Whether to use SSL for the connection. Default is False.
+
+    Attributes:
+        host (str): The host where the Metasploit RPC service is running.
+        port (int): The port to connect to the RPC service.
+        uri (str): The URI path for the RPC service.
+        ssl (bool): Whether to use SSL for the connection.
+        authenticated (bool): Indicates whether the client is authenticated.
+        token: Authentication token.
+        headers (dict): HTTP headers for the RPC request.
+        client: An HTTP(S) connection object.
+
+    Example:
+        Initialize a client with custom options:
+        ```
+        custom_opts = {
+            'host': 'metasploit-server',
+            'port': 55553,
+            'uri': '/api/v1',
+            'ssl': True
+        }
+        client = Msfrpc(custom_opts)
+        ```
+    """
     self.host = opts.get('host') or config('HOST')
     self.port = opts.get('port') or config('PORT')
     self.uri = opts.get('uri') or config('URI')
     self.ssl = opts.get('ssl') or False
-    print(self.ssl)
     self.authenticated = False
     self.token = False
     self.headers = {"Content-type" : "binary/message-pack" }
@@ -34,6 +66,30 @@ class Msfrpc:
     return msgpack.unpackb(data)
 
   def call(self,meth,opts = []):
+    """
+    Call a method on the Metasploit RPC service.
+
+    Args:
+        meth (str): The name of the method to call.
+        opts (list, optional): A list of options to pass to the method. Default is an empty list.
+
+    Returns:
+        dict: A dictionary containing the response from the RPC service.
+
+    Raises:
+        MsfAuthError: If the method requires authentication and the client is not authenticated.
+
+    Example:
+        To call a method named 'module.info' with some options:
+        ```
+        client = Msfrpc()
+        if client.login('your_username', 'your_password'):
+            response = client.call('module.info', ['exploit/multi/http/php_cgi_arg_injection'])
+            print(response)
+        else:
+            print("Authentication failed.")
+        ```
+    """
     if meth != "auth.login":
       if not self.authenticated:
         raise self.MsfAuthError("MsfRPC: Not Authenticated")
@@ -48,7 +104,31 @@ class Msfrpc:
     return self.decode(resp.read()) 
   
   def login(self,user,password):
+    """
+    Authenticate with the Metasploit RPC service using a username and password.
+
+    Args:
+        user (str): The username for authentication.
+        password (str): The password for authentication.
+
+    Returns:
+        bool: True if authentication is successful, False otherwise.
+
+    Raises:
+        MsfAuthError: If authentication fails.
+
+    Example:
+        To authenticate with the Metasploit RPC service:
+        ```
+        client = Msfrpc()
+        if client.login('your_username', 'your_password'):
+            print("Authentication successful.")
+        else:
+            print("Authentication failed.")
+        ```
+    """
     ret = self.call('auth.login',[user,password])
+    print(ret)
     if ret.get(b'result') == b'success':
       self.authenticated = True
       self.token = ret.get(b'token')
@@ -56,22 +136,4 @@ class Msfrpc:
     else:
       raise self.MsfAuthError("MsfRPC: Authentication failed")
 
-if __name__ == '__main__':
-  
-  # Create a new instance of the Msfrpc client with the default options
-  client = Msfrpc({})
-
-  client.login(config('USERNAME'),config('PASSWORD'))
-
-  # Get a list of the exploits from the server
-  mod = client.call('module.exploits')
-  print(mod.get(b"modules")[0])
-  
-  # Grab the first item from the modules value of the returned dict
-  print ("Compatible payloads for : %s\n" % mod[b'modules'][0])
-  
-  # Get the list of compatible payloads for the first option
-  ret = client.call('module.compatible_payloads',[mod[b'modules'][0]])
-  for i in (ret.get(b'payloads')):
-    print ("\t%s" % i)
 
--- 2.Code/Kiennn/metasploit/msfrpc/test.py
+++ 2.Code/Kiennn/metasploit/msfrpc/test.py
@@ -1,21 +1,95 @@
-
+import sys
+import logging
 from pymsfrpc.msfrpc import Msfrpc
 from decouple import config
 
+class AutoExploit(Msfrpc):
+  def search_modules(self, opts):
+    """
+    Search for Metasploit modules by name.
 
-if __name__ == '__main__':
-  # Create a new instance of the Msfrpc client with the default options
-  client = Msfrpc({})
-  client.login(config('USERNAME'),config('PASSWORD'))
+    Args:
+      opts (list of str): A list containing the name of the module(s) to search for.
 
-  # Get a list of the exploits from the server
-  mod = client.call('module.exploits')
-  print(mod.get(b"modules")[0])
-  
-  # Grab the first item from the modules value of the returned dict
-  print ("Compatible payloads for : %s\n" % mod[b'modules'][0])
-  
-  # Get the list of compatible payloads for the first option
-  ret = client.call('module.compatible_payloads',[mod[b'modules'][0]])
-  for i in (ret.get(b'payloads')):
-    print ("\t%s" % i)
+    Returns:
+      list: A list of modules that match the provided module name(s).
+
+    Example:
+      To search for modules with names containing 'exploit/multi/http':
+      matching_modules = self.search_modules(['exploit/multi/http'])
+    """
+    modules = self.call("module.search", opts)
+    # Display modules
+    for module in modules:
+      print(module)
+    return modules
+
+  def show_options(self, opts):
+    """
+    Show the options for a Metasploit module.
+
+    Args:
+        opts (list): A list containing the name of the module. The ModuleName must include module type prefix ("exploit/") or similar.
+
+    Returns:
+        dict: A dictionary containing the module's options.
+
+    Example:
+        To show the options for a module named 'exploit/multi/http/php_cgi_arg_injection':
+        options = self.show_options(['exploit/multi/http/php_cgi_arg_injection'])
+    """
+
+    # convert opts to [Module_type, Module_name]
+    opts = opts[0].split('/', 1)
+    print(opts)
+
+    module_options = self.call("module.options", opts)
+
+    # check if response is error?
+    if module_options.get('error') == True:
+      logging.error(f"Error code {module_options.get('error_code')}")
+      logging.error(f"Error message: {module_options.get('error_message').decode('utf-8')}")
+      sys.exit(1)
+
+    # Display options of module
+    for option, attributes in module_options.items():
+      # option name decode
+      if isinstance(option, bytes):
+        option = option.decode('utf-8')
+      print(f"OPTION: {option}")
+
+      # attribute of option
+      for attribute, attribute_value in attributes.items():
+        # field decode
+        if isinstance(attribute, bytes):
+          attribute = attribute.decode('utf-8')
+        # field value decode
+        if isinstance(attribute_value, bytes):
+          attribute_value = attribute_value.decode('utf-8')
+        print(f"{attribute}: {attribute_value}")
+      print('-' * 40)
+
+    return module_options
+
+  def runTest(self):
+    # arguments is required
+    if len(sys.argv) < 2:
+      logging.error("Tham so bat buoc")
+      sys.exit(1)
+
+    # Get arguments
+    meth = sys.argv[1]
+    opts = sys.argv[2:]
+
+    # Call method
+    if meth == "module.search":
+      modules = self.search_modules(opts)
+      print('=' * 40)
+      module_info = self.show_options([modules[0].get(b'fullname')])
+    elif meth == "module.options":
+      module_info = self.show_options(opts)
+
+if __name__ == "__main__":
+  auto_exploit = AutoExploit({})
+  auto_exploit.login(config('USERNAME'), config('PASSWORD'))
+  auto_exploit.runTest()
